7c6ba3a61706859ea36e15a52b782d0791bc92b0
Robert Seffens <rseffens@gmail.com>
2022-04-26 08:20:41 -0700

    Likes

---
+++ node.js
@@ -0,0 +1,22 @@
+
+var count = 0;
+var countElement = document.querySelector("#count")
+
+function add2() {
+    count++;
+    countElement.innerText = count + " likes";
+}
+var count1 = 0;
+var count1Element = document.querySelector("#count1")
+
+function add1() {
+    count1++;
+    count1Element.innerText = count1 + " likes";
+}
+var count3 = 0;
+var count3Element = document.querySelector("#count3")
+
+function add3() {
+    count3++;
+    count3Element.innerText = count3 + " likes";
+}
